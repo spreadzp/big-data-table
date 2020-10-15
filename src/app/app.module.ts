7de562a7b1c56aee1bf7c30de7476b683b04f4ff
pk <pk@theinvaders.pr>
2020-10-16 00:32:12 +0300

    add paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainTableComponent } from './components/main-table/main-table.component';
-import { SpreadTableComponent } from './components/spread-table/spread-table.component';
 import { DataService } from './services/data.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainTableComponent,
-    SpreadTableComponent
+    MainTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   exports: [
     MatTableModule
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
--- src/app/components/main-table/main-table.component.ts
+++ src/app/components/main-table/main-table.component.ts
@@ -1,27 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-main-table',
   templateUrl: './main-table.component.html',
-  styleUrls: ['./main-table.component.scss']
+  styleUrls: ['./main-table.component.scss'],
 })
 export class MainTableComponent implements OnInit {
- dataTable = null;
- columns = null;
- columnNames = null;
- isMobile = false;
- showTable = false;
+  dataTable = null;
+  columns = null;
+  columnNames = null;
+  isMobile = false;
+  showTable = false;
+  length = 0;
+  pageSize = 10;
+  pageSizeOptions = [10];
 
-  constructor(private dataService: DataService) { }
+  pageEvent: PageEvent;
+  activePageDataChunk = [];
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.getData().subscribe(data => {
+    this.dataService.getData().subscribe((data) => {
       this.dataTable = data.data;
+      this.length = this.dataTable.length;
       this.columns = data.meta.columns;
-      this.columnNames = this.columns.map(item => item.title);
-      console.log("MainTableComponent -> ngOnInit -> this.columnNames", this.columnNames);
-      this.showTable = true;
+      this.columnNames = this.columns.map((item) => item.title);
+      this.activePageDataChunk = this.dataTable.slice(0, this.pageSize);
+      this.showTable = this.length > 0;
     });
+  }
+
+  onPageChanged(e) {
+    if (this.length > 0) {
+      const firstCut = e.pageIndex * e.pageSize;
+      const secondCut = firstCut + e.pageSize;
+      this.activePageDataChunk = this.dataTable.slice(firstCut, secondCut);
+    }
   }
 }
